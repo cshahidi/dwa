d5d01d69b5f2b1969d1ad627a4e4b58dc42ca4c1
Camran Shahidi <cshahidi@post.harvard.edu>
2012-09-18 15:44:19 -0400

    Updated index file (removed html code)

---
+++ index_v1.php
@@ -0,0 +1,33 @@
+<!-- XML declaration should be on first line. Used meta element to specify encoding instead -->
+<!-- <?xml version="1.0" encoding="UTF-8"?> -->
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" >
+
+<head>
+	<title>About Me</title>
+	
+	<!-- Style element fragment copied from Head First book, Starbuzz example -->
+	<style type="text/css">
+	      body {  
+		margin-left: 10%;
+		margin-right: 20%;
+			border: 1px dotted gray;
+		padding: 10px 10px 10px 10px;
+		font-family: sans-serif;
+	      }
+        </style>
+	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+</head>
+
+<body>
+	<h1>Hello World (html)</h1>
+</body>
+</html>
+
+<?php
+	echo "Hello World (php) <br />";
+?>
+
+Hello World (plain)
