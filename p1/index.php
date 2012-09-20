c249a4bacb170876366275fee3ab348cb39493af
Camran Shahidi <cshahidi@post.harvard.edu>
2012-09-20 18:11:52 -0400

    Project 1
    Consists of:
    - index,php
    - style.css
    - images/gloucester.jpg

---
+++ p1/index.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>   <!-- HTML 5 Declaration -->
+
+<html>
+<head>
+	<title>Project 1</title>
+	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+	<link href="style.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+	<div id = "wrapper">
+		<h1>Camran Shahidi</h1>
+		<p>
+			<!-- Image is a hypertext link -->
+			<a href="images/gloucester.jpg"><img src="images/gloucester.jpg" height="450" width="600"
+			alt="My photo" /></a>
+		</p>
+		
+		<h2>Bio</h2>
+		<h3>Early Years</h3>
+		<p>
+		I am an Iranian American born in London, England. My childhood years were spent in London, 
+		Tehran and Geneva, Switzerland. I have lived most of my adult life in Cambridge, with various 
+		stints in Pennsylvania and California.
+		</p>
+		
+		<h3>Education</h3>
+		<p>	
+			I possess both a Bachelor's and Master's degree in Electrical Engineering.  
+			Not content with one Master's degree, I went back to school a few years ago and received a
+			second Master's in Public Adminstration from the  
+			<a href="http://www.hks.harvard.edu/">Harvard Kennedy School</a>.
+			<!-- Could not use target attribute to open link in new window in XHTML 1.0 Strict -->
+		</p>
+		
+		<h3>Career</h3>
+		<p>
+			My career has centered mostly on telecommunications.  I have held various roles in 
+			engineering, professional services and product management at startup companies as well 
+			as larger ones such as Motorola.  Recently, I have diversified and participated in some 
+			real estate projects.
+		</p>
+		<h3>Why This Course?</h3>
+		<p>
+			For professional growth! Web design will be a great addition to my skill set. Morever, I am
+			considering re-orienting my career towards web design and analytics. This course will 
+			provide a solid foundation for that trajectory. 
+		</p>
+		<h3>Hobbies</h3>
+		<p>
+			I am an enthusiastic athlete and love most sports. I regularly enjoy jogging and exercising,
+			and have most recently particpated competitively in the following sports:
+		</p>
+		<ul>
+			<li>Ski racing</li>
+			<li>Tennis</li>
+			<li>Basketball</li>
+		</ul>
+		<br>
+		
+		<h2>Web Host and Plan I am Using</h2>
+		<p> 
+			<a href="http://asmallorange.com/">A Small Orange</a> 
+		</p>
+		<p> 
+			<a href="http://asmallorange.com/hosting/shared">Plan: Shared hosting; Small</a> 
+		</p> 
+
+		<h2>Code Editor I am using</h2>
+		<p>
+			Notepad++
+		</p>
+		<h2>Linked URL to my Git repository</h2>
+		<p>
+			<a href="https://github.com/cshahidi/dwa">https://github.com/cshahidi/dwa</a><br />
+		</p>
+	</div>  <!-- endiv wrapper -->
+</body>
+</html>
+
+	
