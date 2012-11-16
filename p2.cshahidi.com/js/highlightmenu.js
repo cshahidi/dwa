17e1ed014b5b8ea9d597d0e1c57347ead5736210
Camran Shahidi <cshahidi@post.harvard.edu>
2012-11-15 22:00:04 -0500

    Active link fix with doc ready(); css fixes for alignment

--- p2.cshahidi.com/js/highlightmenu.js
+++ p2.cshahidi.com/js/highlightmenu.js
@@ -1,10 +1,13 @@
 /* For all the links in the #menu div, see if the href matches the current url 
-  (window.location.pathname) and if it does, add the class "highlighted".
+  (window.location.pathname) and if it does, set the class "highlighted".
   Source: Susan (on the Forum)
 */
-$("#menu").find("a[href='"+window.location.pathname+"']").each(function() {
-	 $(this).attr('class', 'highlighted');
-});
+
+$(document).ready(function() {	//start doc ready
+	$("#menu").find("a[href='"+window.location.pathname+"']").each(function() {
+		$(this).attr('class', 'highlighted');
+	});
+});	
 
 
 
