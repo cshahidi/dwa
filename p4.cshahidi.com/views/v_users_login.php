7dc951f23b5fa664b8b0af5452af69eefb5b8b30
Camran Shahidi <cshahidi@post.harvard.edu>
2012-12-18 17:22:54 -0500

    P4 - Refined Loginbox and menu highlight style

--- p4.cshahidi.com/views/v_users_login.php
+++ p4.cshahidi.com/views/v_users_login.php
@@ -13,41 +13,40 @@
 	  "/users/p_login" (wouldn't trigger any sort of destination redirect
 -->
 	  
-<div class="login-box">
-	<form id="formID" class="formular" method="POST" action="/users/p_login/<? if(isset($destination)) echo "?destination=".$destination; ?>">		
 
-		<div class="login-info">	
-			<h2>Log in:</h2>
+<form id="formID" class="formular login-box"  method="POST" action="/users/p_login/<? if(isset($destination)) echo "?destination=".$destination; ?>">		
+
+	<div class="login-info">	
+		<h2>Log in:</h2>
+	
+		<p>Email</p>       <!-- validation based on new jQuery Validation plugin -->
+		<input type="text" name="email" class="validate[required,custom[email]] text_input" id="email">
 		
-			<p>Email</p>       <!-- validation based on new jQuery Validation plugin -->
-			<input type="text" name="email" class="validate[required,custom[email]] text_input" id="email">
-			
-			<p>Password</p>
-			<input type="password" name="password" class="validate[required] text_input" id="password">
+		<p>Password</p>
+		<input type="password" name="password" class="validate[required] text_input" id="password">
+		<br>
+		
+		<!-- Insert in $_POST the $role variable ("partner" vs "principal") that login() has set -->			
+		<input type="hidden" name="role" value="<?=$role?>">			
+
+		<!-- $error defined in login() method; not defined in index() method for landing page. Suppress it -->	
+		<? if(@$error): ?>    
+			<div class="error">
+				Login failed. Please double check your email and password.
+			</div>
 			<br>
-			
-			<!-- Insert in $_POST the $role variable ("partner" vs "principal") that login() has set -->			
-			<input type="hidden" name="role" value="<?=$role?>">			
-
-			<!-- $error defined in login() method; not defined in index() method for landing page. Suppress it -->	
-			<? if(@$error): ?>    
-				<div class="error">
-					Login failed. Please double check your email and password.
-				</div>
-				<br>
-			<? endif; ?>	
-			
-			<input type="submit" value="Log In"><br>
-			
-			<!-- $role defined in login() method: Either partner or principal -->			
-			<p>New User (<?=$role?>)?</p>
-			<span id="signup"><a href="/users/signup/<?=$role?>">Signup</a></span>	
-			
-		</div> <!-- endiv login-info -->	
+		<? endif; ?>	
 		
+		<input type="submit" value="Log In"><br>
+		
+		<!-- $role defined in login() method: Either partner or principal -->			
+		<p>New User (<?=$role?>)?</p>
+		<span id="signup"><a href="/users/signup/<?=$role?>">Signup</a></span>	
+		
+	</div> <!-- endiv login-info -->	
+	
+</form>
 
-	</form>
-</div>
 
 
 <!-- Use new jQuery validation Plugin script -->
